4d0bd784722736a065ac2cb284d25c66766fc270
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2022-01-05 17:38:45 +0700

    test

--- lint-staged.js
+++ lint-staged.js
@@ -1,7 +1,6 @@
 module.exports = {
-   '*.{js,jsx,ts,tsx}': [
-      'eslint --max-warnings=0 --fix',
-      'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests'
-   ],
-   'src/*.{ts,tsx,css}': ['prettier --write']
+  '*.{js,jsx,ts,tsx}': [
+    'eslint --max-warnings=0 --fix',
+    'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests'
+  ]
 };
