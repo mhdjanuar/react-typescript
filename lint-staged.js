b883abaf5b80b2d53896eb81ad8c46ea1a4b9671
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2022-01-05 18:15:54 +0700

    clean

--- lint-staged.js
+++ lint-staged.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [
+  '*.{js,jsx,ts,tsx,css}': [
     'eslint --max-warnings=0 --fix',
     'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests'
   ]
